306c8f6387a98d920e11a4e7dab765387f7b6ab5
Anish6264 <sharanishsharma@gmail.com>
2026-05-15 00:24:51 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        success:true,
+        message:"Hello World"
+    });
+})
+
+app.listen(ENV.PORT,()=>{
+    console.log(`Server is running on port ${ENV.PORT}`);
+})
+
